41d5ec381e56fa2b2acb97382a2a454a63947a42
Merve <merve.oktay@hotmail.com>
2023-06-02 18:43:32 +0300

    firstCommit

--- app/src/main/java/com/example/merve_oktay_odev3/services/UserAboutService.kt
+++ app/src/main/java/com/example/merve_oktay_odev3/services/UserAboutService.kt
@@ -6,9 +6,9 @@ class UserAboutService {
     fun  userAboutData():UserAbout
     {
         val  about="Ben Merve OKTAY. Android programlama üzerine kendimi geliştiriyorum. Çeşitli alanlarda projerler yapma imkanım oldu. " +
-                "Üniversitede görüntü işleme projesi, yapay sinir ağları ile görüntü üzerinde sınıflandırma ve doğal dil işleme projeleri geliştirdim."+
-                "Üniversiteden sonra java ve C# eğitimleri aldım. Bu eğitimlerde .Net projeleri geliştirdim." +
-                " Aldığım eğitimler ve yapltığım projerler sonrasında Android uygulama geliştirmenin benim için daha iyi olduğuna ve severek öğrendime kanaat getirdiğim için bu alanda ilerlemeye karar verdim."
+                "Üniversitede görüntü işleme projesi, yapay sinir ağları ile görüntü sınıflandırma ve doğal dil işleme projeleri geliştirdim."+
+                "Üniversiteden sonra java ve C# eğitimleri alıp .Net projeleri geliştirdim." +
+                " Aldığım eğitimler ve yapltığım projerler sonrasında Android uygulama geliştirmenin benim için daha iyi olduğuna ve severek öğrendiğimi düşündüğüm için bu alanda ilerlemeye karar verdim."
         val education=listOf("Anadolu ticaret Meslek Lisesi / Veri Tabanı Programcılığı ","Kocaeli Üniversitesi / Bilgisayar Programcılığı","Karadeniz Teknik Üniversitesi / Bilgisayar Mühendisliği")
         val experience=listOf("Kocaeli Büyükşehir Belediyesi /  Bilgi İşlem Dairesi")
         val  skills = listOf("C++","C#","Java","Kotlin","SQL","Görüntü İşleme", "Yapay Sinir Ağları")
